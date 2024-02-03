5bacd280ae0d98c82a78187904e9270cffd9a83b
hatsumi3 <al.usamiku39@gmail.com>
2024-02-03 20:27:06 +0900

    :hammer: add/ modify css.

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,15 +1,24 @@
 import { useState } from "preact/hooks";
 
 export default function Greeting({ messages }) {
+  const style = {
+    margin: "10px auto",
+    color: "white",
+    backgroundColor: "#4c1d95",
+    textDecoration: "none",
+    padding: "0.25rem 0.5rem",
+    borderRadius: "5px",
+  };
   const randomMessage = () =>
     messages[Math.floor(Math.random() * messages.length)];
-
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
     <div>
       <h3>{greeting}！ 訪問いただきありがとうございます！</h3>
-      <button onClick={() => setGreeting(randomMessage())}>新しい挨拶</button>
+      <button {...{ style }} onClick={() => setGreeting(randomMessage())}>
+        新しい挨拶
+      </button>
     </div>
   );
 }
